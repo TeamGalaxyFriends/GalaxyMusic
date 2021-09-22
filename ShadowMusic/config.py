01f28acdc9253e79932ced56fe28aa4e704c0fa5
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-09-22 16:47:56 +0530

    Lo

--- ShadowMusic/config.py
+++ ShadowMusic/config.py
@@ -1,4 +1,5 @@
-# Shadow Music - Telegram bot project
+# Galaxy Music - Telegram bot project
+
 # Copyright (C) 2021 Roj Serbest
 # Copyright (C) 2021 Deshadeeth Thisarana
 # This program is free software: you can redistribute it and/or modify
