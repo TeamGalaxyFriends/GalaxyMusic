f8b3d0befb94faaeceefe12f0429cea2781c5595
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-09-22 17:33:08 +0530

    Galaxy Music

--- GalaxyMusic/services/converter/converter.py
+++ GalaxyMusic/services/converter/converter.py
@@ -18,7 +18,7 @@
 import asyncio
 from os import path
 
-from ShadowMusic.helpers.errors import FFmpegReturnCodeError
+from GalaxyMusic.helpers.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
