e3ab503240a76cdae044a869b835c60b7eda47b5
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-09-22 18:44:28 +0530

    Galaxy Music

--- GalaxyMusic/modules/pmpermit.py
+++ GalaxyMusic/modules/pmpermit.py
@@ -1,4 +1,4 @@
-# Shadow Music (Telegram bot project )
+# Galaxy Music (Telegram bot project )
 # Copyright (C) 2021 Deshadeeth Thisarana
 
 # This program is free software: you can redistribute it and/or modify
