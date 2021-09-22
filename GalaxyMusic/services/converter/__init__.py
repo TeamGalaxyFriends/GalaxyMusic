a33c0f743633b3cb205e7a768ebc4b43efad580b
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-09-22 17:32:12 +0530

    Galaxy Music

--- GalaxyMusic/services/converter/__init__.py
+++ GalaxyMusic/services/converter/__init__.py
@@ -3,6 +3,6 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from ShadowMusic.services.converter.converter import convert
+from GalaxyMusic.services.converter.converter import convert
 
 __all__ = ["convert"]
