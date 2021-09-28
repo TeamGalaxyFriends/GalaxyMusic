3e2047b11905afce20b980280dfbc74659180a1e
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-09-28 14:33:48 +0530

    Galaxy Music

--- str.py
+++ str.py
@@ -7,7 +7,7 @@ print("Enter your app information from my.telegram.org/apps below.")
 
 async def main():
     async with Client(
-        ":memory:", api_id=int(input("API ID:")), api_hash=input("API HASH:")
+        "GalaxyMusic", api_id=int(input("API ID:")), api_hash=input("API HASH:")
     ) as app:
         print(await app.export_session_string())
 
